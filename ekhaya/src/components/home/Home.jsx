5c8e9f182e044694c66fa9ca81ee80c9a2f22029
Siyabonga Mbuyisa-20491621 <160500265+Siyabonga-Mbuyisa-u20491621@users.noreply.github.com>
2025-01-18 23:40:31 +0200

    Animation working

--- ekhaya/src/components/home/Home.jsx
+++ ekhaya/src/components/home/Home.jsx
@@ -6,17 +6,22 @@ import Typewriter from 'typewriter-effect';
 export default function Home() {
     return (
       <div className='HomePage'>
-        <p>
-           
-        </p>
+        <div className='AnimatedText'>
         <Typewriter
-            options={{
-                strings: [' I\'m Siyabonga Mbuyisa', ],
-                autoStart: true,
-                loop: false,
+            onInit={(typewriter) => {
+                typewriter.typeString('<span style="font-size: 50px;">I \'m Siyabonga Mbuyisa')
+                    .callFunction(() => {
+                        console.log('First string typed out!');
+                    })
+                    .pauseFor(800)
+                    .typeString('<br/><span style="font-size: 30px;">Software Engineer')
+                    .callFunction(() => {
+                        console.log('Second string typed out!');
+                    })
+                    .start();
             }}
         />
-              
+        </div>     
        
       </div>
     );
