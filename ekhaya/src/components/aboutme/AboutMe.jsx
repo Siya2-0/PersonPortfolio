aa53b0ea8bf62c736893b02269680636493cc778
Siyabonga Mbuyisa-20491621 <160500265+Siyabonga-Mbuyisa-u20491621@users.noreply.github.com>
2025-01-23 21:19:22 +0200

    Added AboutME and Skills content. They need styling

--- ekhaya/src/components/aboutme/AboutMe.jsx
+++ ekhaya/src/components/aboutme/AboutMe.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import "./AboutMe.css";
+import profileImage from '../../img/ID photo.jpeg'
 
 export default function AboutMe() {
     return (
@@ -9,12 +10,47 @@ export default function AboutMe() {
       <div className="about-me-container">
       
             <div className="section1">
-            Hi, I'm Siyabonga Mbuyisa, a software developer based in South Africa. My long-term goal is to participate in life-changing projects.</div>
-            <div className="right-container">
-                <div className="section2">In 2025, I will be pursuing my Honours studies. I have a passion for software engineering, cybersecurity, and artificial intelligence. My journey through software development has been marked by perseverance, academic excellence, and a drive to create impactful solutions. I am committed to expanding my skills and becoming a top-tier software engineer, ready to tackle complex challenges in the industry.</div>
-                <div className="section3">Lets Connect</div>
+            <img src={profileImage} alt="Profile" className="profile-image" />
+            <div class="blink-animation-container"> 
+                
+                <span className ='blink-animation'></span>
+                Available for New Projects
+
+
             </div>
+            Hi, I'm Siyabonga Mbuyisa, a software developer based in South Africa. My long-term goal is to participate in life-changing projects.
+            </div>
+           
+            <div className="section2">In 2025, I will be pursuing my Honours studies. I have a passion for software engineering, cybersecurity, and artificial intelligence. My journey through software development has been marked by perseverance, academic excellence, and a drive to create impactful solutions. I am committed to expanding my skills and becoming a top-tier software engineer, ready to tackle complex challenges in the industry.</div>
+            <div className="section3">
+                <h3>Interests</h3>
+              
+                <div className='custom-border'>Soccer</div>
+                <div className='custom-border'>Health and Fitness</div>
+                <div className='custom-border'>Artificial Intelligence</div>
+                <div className='custom-border'>Cyber Security</div>
+
+            </div>
+            <div className="section4"> 
+                <h3>Let connect</h3>
+
+                0731059725<br></br>
+                siyabongambuyisa7@gmail.comm<br></br>
+                linkedin
+            </div>
+           
         </div>
-      </>
+        </>
     );
   }
+
+
+//   <div className="about-me-container">
+      
+//             <div className="section1">
+//             Hi, I'm Siyabonga Mbuyisa, a software developer based in South Africa. My long-term goal is to participate in life-changing projects.</div>
+//             <div className="right-container">
+//                 <div className="section2">In 2025, I will be pursuing my Honours studies. I have a passion for software engineering, cybersecurity, and artificial intelligence. My journey through software development has been marked by perseverance, academic excellence, and a drive to create impactful solutions. I am committed to expanding my skills and becoming a top-tier software engineer, ready to tackle complex challenges in the industry.</div>
+//                 <div className="section3">Lets Connect</div>
+//             </div>
+//         </div>
--- ekhaya/src/components/skills/Skills.jsx
+++ ekhaya/src/components/skills/Skills.jsx
@@ -6,13 +6,27 @@ export default function Skills() {
     return (
       <div className='SkillsPage'>
 
-        <div className='Skills'>
-          <h1 className='SectionTitle'>Skills</h1>
-          
+      <div className='Skills'>
+            <h1 className='SectionTitle'>Skills</h1>
+            <p>Proficient in the following technologies:</p>
+            <ul className='SkillsList'>
+                <li>JavaScript</li>
+                <li>Java</li>
+                <li>Python</li>
+                <li>C++</li>
+                <li>Django</li>
+                <li>Android</li>
+                <li>React</li>
+            </ul>
+            <p>Also capable of learning new languages or frameworks.</p>
         </div>
         <div className='Certification'>
-          <h1 className='SectionTitle'>Certification</h1>
-
+                <h1 className='SectionTitle'>Certification</h1>
+                <ul className='CertificationList'>
+                    <li>AWS Cloud Practitioner</li>
+                    <li>AWS Serverless</li>
+                    <li>BSc Computer Science</li>
+                </ul>
         </div>
        
       </div>
