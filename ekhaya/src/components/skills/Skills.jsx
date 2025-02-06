877449e1e4155a3f0d564a2b3a55ceb574b00c20
Siyabonga Mbuyisa-20491621 <160500265+Siyabonga-Mbuyisa-u20491621@users.noreply.github.com>
2025-02-06 23:39:37 +0200

    horizontal scroll not complete

--- ekhaya/src/components/skills/Skills.jsx
+++ ekhaya/src/components/skills/Skills.jsx
@@ -1,23 +1,94 @@
 
 import React from 'react';
 import "./Skills.css";
+import javascript from '../../img/java_script.png'
+import java from '../../img/java.png'
+import python from '../../img/python.png'
+import cplusplus from '../../img/cplusplus.png'
+import css from '../../img/css.png'
+import android from '../../img/android.png'
+import django from '../../img/Django_logo.svg (1).png'
+import html from '../../img/html.png'
+import react from '../../img/react.png'
+
+
+
+
+
+
+
 
 export default function Skills() {
     return (
-      <section className='pinned'>
+      <section className='skillspinned'>
+        <h1>My Skills and Certification.</h1>
 
-      <div className="carousel">
-        <div className="group-one">
-           <div className="card animate">
+      <div className="carousel1">
+      
+        <div className="item item1">
             <div className="skill-logo">
-              <img src='' atl></img>
+              <img src={javascript} alt='Javascript'></img>
             </div>
-            <div className="skill-name"></div>
-           </div>
+            <div className="skill-name">JavaScript</div>
         </div>
+
+        <div className="item item2">
+            <div className="skill-logo">
+              <img src={java}alt='Java'></img>
+            </div>
+            <div className="skill-name">Java</div>
+        </div>
+
+        <div className="item item3">
+            <div className="skill-logo">
+              <img src={python} alt='Python'></img>
+            </div>
+            <div className="skill-name">Python</div>
+        </div>
+
+        <div className="item item4">
+            <div className="skill-logo">
+              <img src={cplusplus} alt='C++'></img>
+            </div>
+            <div className="skill-name">C++</div>
+        </div>
+        <div className="item item5">
+            <div className="skill-logo">
+              <img src={css} alt='CSS'></img>
+            </div>
+            <div className="skill-name">CSS</div>
+        </div>
+
+        <div className="item item6">
+            <div className="skill-logo">
+              <img src={android} alt='Android'></img>
+            </div>
+            <div className="skill-name">Android</div>
+        </div>
+
+        <div className="item item7">
+            <div className="skill-logo">
+              <img src={react} alt='React'></img>
+            </div>
+            <div className="skill-name">React</div>
+        </div>
+
+
+        <div className="item item8">
+            <div className="skill-logo">
+              <img src={html} alt='html'></img>
+            </div>
+            <div className="skill-name">HTML</div>
+        </div>
+      
       </div>
 
-      <div className='Skills'>
+
+ 
+      
+        
+
+      {/* <div className='Skills'>
             <h1 className='SectionTitle'>Skills</h1>
             <p>Proficient in the following technologies:</p>
             <ul className='SkillsList'>
@@ -38,7 +109,7 @@ export default function Skills() {
                     <li>AWS Serverless</li>
                     <li>BSc Computer Science</li>
                 </ul>
-    </div>
+    </div> */}
        
     </section>
     );
